a142683704134771efab079bf3409e7320562b8e
Arshanashraf <arshanashraf2002@gmail.com>
2024-09-11 16:40:57 +0500

    configuring redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 
+import { useState,useEffect } from 'react'
 import './App.css'
-
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+ // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispath= useDispatch()
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData){
+        dispath(login([userData]))
+      }else{
+        dispath(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+        TODO:  {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,57 +1,62 @@
-import conf from '../conf/conf'
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
 
-import { Client,Account,ID } from 'appwrite'
 
-export class AuthService{
+export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId)
+            .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
+            
     }
 
-    async createAccount({email,password,name}){
+    async createAccount({email, password, name}) {
         try {
-            const userAccount = await this.account.create(ID.unique(), email,password,name);
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
             if (userAccount) {
-                //call another method
-                return this.login({email,password});
+                // call another method
+                return this.login({email, password});
             } else {
-                return userAccount;
+               return  userAccount;
             }
         } catch (error) {
             throw error;
         }
     }
 
-    async login({email,password}){
+    async login({email, password}) {
         try {
-            return await this.account.createEmailSession(email,password);
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
             throw error;
         }
     }
-    async getCurrentUser(){
+
+    async getCurrentUser() {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error" , error);
-            
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
     }
-    async logout(){
+
+    async logout() {
+
         try {
-           await this.account.deleteSessions()
+            await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: logout :: error" ,error);
-            
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
 const authService = new AuthService();
+
+export default authService
 
