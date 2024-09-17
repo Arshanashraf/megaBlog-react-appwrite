c84bbfc1869574f8df4cf2ccb70190a3342c2613
Arshanashraf <arshanashraf2002@gmail.com>
2024-09-17 16:43:20 +0500

    Building All pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,21 @@ import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LOgoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 export {
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 }
